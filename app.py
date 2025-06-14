a97ce4224e2383183d9733517afa60422df15bed
Fykio <olufemi.akinboye@darey.io>
2025-06-14 20:31:03 +0100

    to trigger action

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Universe! This is a my simple app built using Flask.'
+    return 'Hello, Universe! This is a my simple app built using Flask. A class project.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6565, debug=True)
